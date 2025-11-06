def6f202b4b5eec373a7c738b572c0a96bb19b25
wewewer4fteeeeeee <151419080+wewewer4fteeeeeee@users.noreply.github.com>
2025-11-06 05:31:35 -0600

    Update leathaapkefagthing.lua

--- lua/leathaapkefagthing.lua
+++ lua/leathaapkefagthing.lua
@@ -4,47 +4,47 @@ local replicatedStorage = game:GetService("ReplicatedStorage")
 local tweenService = game:GetService("TweenService")
 
 task.spawn(function()
-    repeat
-        local ok = pcall(function()
-            game:GetService("StarterGui"):SetCore("SendNotification",{
-                Title = "",
-                Text = "Made by Exploding_Car/Acethecreator (Discord)\nStop skidding, you script kiddies",
-                Duration = 555
-            })
-        end)
-        if ok then break end
-        task.wait(0.5)
-    until ok
+	repeat
+		local ok = pcall(function()
+			game:GetService("StarterGui"):SetCore("SendNotification",{
+				Title = "",
+				Text = "Made by Exploding_Car/Acethecreator (Discord)\nStop skidding, you script kiddies",
+				Duration = 555
+			})
+		end)
+		if ok then break end
+		task.wait(0.5)
+	until ok
 end)
+
 task.spawn(function()
-    repeat
-        local ok = pcall(function()
-            game:GetService("StarterGui"):SetCore("SendNotification",{
-                Title = "",
-                Text="How to use\nR to spam kill and yeah that is it",
-                Duration = 555
-            })
-        end)
-        if ok then break end
-        task.wait(0.5)
-    until ok
-	end
+	repeat
+		local ok = pcall(function()
+			game:GetService("StarterGui"):SetCore("SendNotification",{
+				Title = "",
+				Text = "How to use\nR to spam kill and yeah that is it",
+				Duration = 555
+			})
+		end)
+		if ok then break end
+		task.wait(0.5)
+	until ok
 end)
 
 setclipboard("discord.gg/FGRmuKR2")
 task.wait(1.0)
 task.spawn(function()
-    repeat
-        local ok = pcall(function()
-            game:GetService("StarterGui"):SetCore("SendNotification",{
-                Title = "",
-                Text="Discord Copied Join For More Scripts",
-                Duration = 555
-            })
-        end)
-        if ok then break end
-        task.wait(0.5)
-    until ok
+	repeat
+		local ok = pcall(function()
+			game:GetService("StarterGui"):SetCore("SendNotification",{
+				Title = "",
+				Text = "Discord Copied Join For More Scripts",
+				Duration = 555
+			})
+		end)
+		if ok then break end
+		task.wait(0.5)
+	until ok
 end)
 
 local localgooner = non67mangoabusers.LocalPlayer
@@ -77,4 +77,3 @@ pconlylololoolll.InputBegan:Connect(function(input, gameProcessed)
 		end
 	end
 end)
-
